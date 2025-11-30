96bde15a9ec6a6865f67f772e784dc68240481b4
Karan5663 <132004682+Karan5663@users.noreply.github.com>
2025-11-30 22:13:13 +0530

    Appointment book and Appointment view Fuctionality completed

--- src/App.jsx
+++ src/App.jsx
@@ -24,8 +24,9 @@ import Unauthorized from "./pages/UnAuthorized/UnAuthorized";
 import ViewBloodBank from "./features/doctors/pages/ViewBloodBank"
 
 //Patient Pages
-import BloodBankTab from "./components/BloodBank/BloodBankTab";
+import BloodBankTab from "./features/BloodBank/BloodBankTab";
 import ViewDoctorsPage from "./features/Patient/ViewDoctor/ViewDoctorPage";
+import Appointments from "./features/Patient/Appointment/ManageAppointments";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -109,6 +110,7 @@ const router = createBrowserRouter([
               children: [
                 { path: "bloodbank", element: <BloodBankTab /> },
                 { path: "viewdoctor", element: <ViewDoctorsPage /> },
+                {path: "viewappointments", element: <Appointments/> }
               ],
             },
           ],
