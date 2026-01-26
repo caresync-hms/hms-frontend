6f48fd04df51f39651d1476648a498c92bc8ca4c
Prathmesh Aher <prathameshaher456@gmail.com>
2026-01-26 19:24:02 +0530

    admin - patient functionality complete

--- src/services/patientsApi.js
+++ src/services/patientsApi.js
@@ -16,6 +16,18 @@ export const patientsApi = api.injectEndpoints({
       invalidatesTags: ["Patient"],
     }),
 
+    updatePatient: builder.mutation({
+      query: ({ id, ...data }) => ({
+        url: `/patient/${id}`,
+        method: "PUT",
+        body: data,
+      }),
+      invalidatesTags: (result, error, { id }) => [
+        { type: "Patient", id },
+        "Patient",
+      ],
+    }),
+
     updatePatientStatus: builder.mutation({
       query: ({ patientId, status }) => ({
         url: `/patient/${patientId}/status`,
@@ -38,6 +50,7 @@ export const patientsApi = api.injectEndpoints({
 export const {
   useGetAllPatientsQuery,
   useAddPatientMutation,
+  useUpdatePatientMutation,
   useUpdatePatientStatusMutation,
   useDeletePatientMutation,
 } = patientsApi;
