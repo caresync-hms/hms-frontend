0bbb7ea02c4a04f40b4cf4c255890404eb7ab79c
Prathmesh Aher <prathameshaher456@gmail.com>
2026-01-31 01:48:52 +0530

    pls work

--- src/pages/UnAuthorized/UnAuthorized.jsx
+++ src/pages/UnAuthorized/UnAuthorized.jsx
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
-import { Icons } from "../../assets/Icons";
+import { Icons } from "@/assets/Icons";
 
 function UnAuthorized() {
   const navigate = useNavigate();
