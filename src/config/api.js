21be04133ec592c100fb0f6247cad0a58ad32ecb
Prathmesh Aher <prathameshaher456@gmail.com>
2026-01-31 00:54:24 +0530

    Deploy frontend via Docker & GitHub Actions

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
 // export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-export const API_BASE_URL = "http://localhost:9093";
+export const API_BASE_URL = "http://13.50.243.68:80";
 
 // const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 // const BLOODBANK_URL = import.meta.env.VITE_BLOODBANK_API_URL;
